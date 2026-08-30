71e3e73731d734389439c3ed828497ad8a4ae2a9
Taimoor Shehzad <taimoorahmedshehzad@gmail.com>
2026-08-30 15:38:41 +0500

    succesfully added login endpoint

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -65,7 +65,37 @@ router.post('/register',async(req,res)=>{
  })
 
 router.post('/login',async(req,res)=>{
-  res.send('Login page')
+  try {
+    const {email,password}= req.body
+    if(!email || !password){
+      return res.status(400).json({message: 'All fields are required'})
+    }
+
+    const user=await User.findOne({email})
+    if(!user){
+      return res.status(400).json({message: "Invalid credentials"})
+    }
+
+    const isPasswordCorrect= await user.comparePassword(password)
+    if(!isPasswordCorrect){
+      return res.status(400).json({message: "Invalid credentials"})
+    }
+
+    const token=generateToken(user._id)
+    res.status(200).json({
+      token,
+      user: {
+        id: user._id,
+        username: user.username,
+        email: user.email,
+        profileImage: user.profileImage
+      }
+    })
+
+  } catch (error) {
+    console.log('Error during login', error);
+    res.status(500).json({message: 'Internal server error'})
+  }
 })
 
 export default router
