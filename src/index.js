01f8e978bc5a491c0a294e9300ae7232243d0c9d
Taimoor Shehzad <taimoorahmedshehzad@gmail.com>
2026-09-02 12:32:36 +0500

    Setup cron jobs

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import 'dotenv/config'
 import authRoutes from './routes/authRoutes.js'
 import bookRoutes from './routes/bookRoutes.js'
 import { connectDB } from './lib/db.js'
+import job from './lib/CRON.JS'
 
 
 const app = express()
 const PORT = process.env.PORT || 3000
 
+job.start()
 app.use(express.json())
 app.use('/api/auth',authRoutes)
 app.use('/api/books',bookRoutes)
