62bf417e1aa24881dc50c22af861d50c33eaef71
Taimoor Shehzad <taimoorahmedshehzad@gmail.com>
2026-09-05 06:22:59 +0500

    fixed user createdAt not going in response

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -53,7 +53,8 @@ router.post('/register',async(req,res)=>{
         id: user._id,
         username: user.username,
         email: user.email,
-        profileImage: user.profileImage
+        profileImage: user.profileImage,
+        createdAt: user.createdAt
       }, 
     })
 
@@ -88,7 +89,8 @@ router.post('/login',async(req,res)=>{
         id: user._id,
         username: user.username,
         email: user.email,
-        profileImage: user.profileImage
+        profileImage: user.profileImage,
+        createdAt: user.createdAt
       }
     })
 
